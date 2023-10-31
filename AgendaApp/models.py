0d9306b6739a7d7e882ca9935c8dea467e192e76
Rebeca Rezende <rebeca.rezende7@gmail.com>
2023-10-30 21:36:44 -0300

    Versão Inicial do Projeto Agenda

---
+++ AgendaApp/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Agenda(models.Model):
+
+    estados_civis = [
+        ('S', 'Solteiro'),
+        ('C','Casado'),
+        ('D','Divorciado'),
+        ('V','Viúvo')
+        ]
+
+    nome = models.CharField(max_length=100)
+    apelido = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)
+    DataNascimento = models.DateField(verbose_name="Data de Nascimento")
+    endereco = models.CharField(max_length=200, verbose_name='Endereço')
+    numero = models.CharField(max_length=10, verbose_name='Número')
+    complemento = models.CharField(max_length=50, null=True, blank=True)
+    cep = models.CharField(max_length=9)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=50)
+
+
+
+    def __str__ (self):
+        return self.nome
