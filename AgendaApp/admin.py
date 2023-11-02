55737aaced0ccce5136134e87aeed995d215b29b
Rebeca Rezende <rebeca.rezende7@gmail.com>
2023-11-01 22:17:14 -0300

    Adicionando classe telefones

--- AgendaApp/admin.py
+++ AgendaApp/admin.py
@@ -2,7 +2,22 @@ from django.contrib import admin
 from AgendaApp.models import Agenda
 from AgendaApp.models import Cidade
 from AgendaApp.models import Telefone
+
 # Register your models here.
-admin.site.register(Agenda)
+
+class Telefones(admin.StackedInline):
+    model = Telefone
+
+
+
+class AgendaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'apelido', 'DataNascimento']
+    list_filter = ['DataNascimento', 'cidade', 'estado']
+    search_fields = ['nome', 'apelido']
+    inlines = [Telefones]    
+
+
+
+admin.site.register(Agenda, AgendaAdmin) 
 admin.site.register(Cidade)
 admin.site.register(Telefone)
