518aff8c65e23e63ce2fdf90a2552aa0523566db
Rebeca Rezende <rebeca.rezende7@gmail.com>
2023-11-08 19:45:29 -0300

    Alteração do banco de dados

--- Agenda/settings.py
+++ Agenda/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+
+pymysql.version_info = (1, 4, 6, 'final', 0)
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'Agenda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'agenda',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   
+        'PORT': '3306',
     }
 }
 
